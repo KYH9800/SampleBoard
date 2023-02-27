19e5f7d18d890e10749c3e81e8473fb328272f63
고윤혁 <sksgur3217@gmail.com>
2023-02-27 13:58:40 +0900

    :sparkles: [feat] 메인페이지 CSS 80% 완료, 채팅 박스 작업중

--- front/components/Banner/BannerIndexTop.js
+++ front/components/Banner/BannerIndexTop.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 // styled-components
-import { BoxWrapper } from '../../styles/pages/HomeSt';
 import { BannerImg } from '../../styles/components/Banners/BannerIndexTopSt';
 
 // index top
