73647a9313379527ef7e77d336e6f5c990b82809
고윤혁 <sksgur3217@gmail.com>
2023-02-28 04:18:20 +0900

    :sparkles: [feat] 갤러리 및 유튜브 포스트 카드 CSS 완성

--- front/components/Pages/PostCardEntry.js
+++ front/components/Pages/PostCardEntry.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 // styled-components
 import { Dot, ImgSvg } from '../../styles/pages/BoardEntrySt';
-import styled from 'styled-components';
+import { ImageBox, PostInfo, TitleContainer, PostImg, ImageEtc } from '../../styles/pages/PostCardEntry';
 
 const PostCardEntry = ({ data }) => {
   return (
@@ -12,9 +12,9 @@ const PostCardEntry = ({ data }) => {
           <PostImg src={data.src} />
         </ImageBox>
         <PostInfo id="info">
-          <div id="titleContainer">
+          <TitleContainer id="titleContainer">
             <span>{data.title}</span>
-          </div>
+          </TitleContainer>
           <ImageEtc id="title-etc">
             <ImgSvg src="/images/10.svg" />
             <div id="작성자">{data.user_name}</div>
@@ -30,35 +30,3 @@ const PostCardEntry = ({ data }) => {
 };
 
 export default PostCardEntry;
-
-const ImageBox = styled.div`
-  position: relative;
-  width: 100%;
-  padding: 0;
-  padding-bottom: 78.43%;
-  margin: 0;
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-`;
-
-const PostInfo = styled.div`
-  padding: 20px;
-  border-bottom-left-radius: 8px;
-  border-bottom-right-radius: 8px;
-`;
-
-const PostImg = styled.img`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  border-top-left-radius: 8px;
-  border-top-right-radius: 8px;
-`;
-
-const ImageEtc = styled.div`
-  display: flex;
-  align-items: center;
-
-  color: #9da5b6;
-`;
--- front/styles/pages/PostCardEntry.js
+++ front/styles/pages/PostCardEntry.js
@@ -12,8 +12,54 @@ export const PostCardList = styled.div`
 
     display: flex;
     flex-direction: column;
+
     border-radius: 8px;
+    padding-bottom: 9px;
+
     background-color: #fff;
     box-shadow: 0 2px 30px 0 rgb(0 0 0 / 6%);
   }
+`;
+
+/*************************
+ * PostCardEntry
+ *************************/
+export const ImageBox = styled.div`
+  position: relative;
+  width: 100%;
+  padding: 0;
+  padding-bottom: 78.43%;
+  margin: 0;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+`;
+
+export const PostInfo = styled.div`
+  padding: 20px;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+`;
+
+export const TitleContainer = styled.div`
+  margin-bottom: 10px;
+
+  font-weight: 500;
+`;
+
+export const PostImg = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+`;
+
+export const ImageEtc = styled.div`
+  display: flex;
+  align-items: center;
+
+  color: #9da5b6;
+
+  font-size: 13.5px;
 `;
